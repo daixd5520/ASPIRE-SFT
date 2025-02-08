48e8c1fa6195cee44d6f54393bdcc9a0f32ad62b
xindi dai <73395671+daixd5520@users.noreply.github.com>
2025-02-08 17:26:00 +0800

    Update calc_aspires_data.py

--- strategy/calc_aspires_data.py
+++ strategy/calc_aspires_data.py
@@ -5,8 +5,6 @@ with open('../score_res/raw/llama2-7b_raw_wizard.pkl', 'rb') as f:
 with open('../score_res_ground-truth/wizard-70k/scoring_res.pkl','rb') as f:
     gt_score = pickle.load(f)
 
-print("ASPIRE-D数据生成完成!")
-
 import pandas as pd
 import json
 import os
@@ -19,7 +17,7 @@ score_df=pd.DataFrame(score)
 data = {}
 for index, row in score_df.iterrows():
     idx = row['Index']
-    score = row['res得分']
+    score = row['resScore']
     if idx not in data:
         data[idx] = {}
     data[idx]["raw_score"] = score
@@ -27,7 +25,7 @@ for index, row in score_df.iterrows():
 
 for index, row in gt_score_df.iterrows():
     idx = row['Index']
-    score = row['res得分']
+    score = row['resScore']
     data[idx]["gt_score"] = score
     print(f"data[{idx}]['gt_score'] = {score}")
 
@@ -39,8 +37,7 @@ for idx, scores in data.items():
     score_diff.append([idx, diff])
 
 score_diff_df = pd.DataFrame(score_diff, columns=["Index", "score_diff"])
-
-# 排序
+# sort
 score_diff_df = score_diff_df.sort_values(by="score_diff", ascending=True)
 print(score_diff_df)
 
@@ -54,6 +51,3 @@ sorted_original_data = [original_data[idx] for idx in score_diff_df["Index"]]
 output_file='../datasets/sft/wizard_llama2-13b_strategy2.json'
 with open(output_file, "w") as f:
     json.dump(sorted_original_data, f, indent=4)
-
-# print(f"{output_file} 行数: {len(remaining_data)}")
-# print("处理完成")
